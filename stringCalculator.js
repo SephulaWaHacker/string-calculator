e919138484d7b11c6bf39a567bf154c9f8cc2335
decentguy <bludd1one@gmail.com>
2019-06-03 15:22:56 +0200

    hopefully done

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,41 +1,31 @@
-class StringCalculator{
-  constructor(){
-    this.add = (string) => {
-      let split;
+class StringCalculator {
+  constructor() {
+    this.add = string => {
+      let arr;
       console.log(`string = ${string}`);
-      let normalise = string.replace(/(\%|\,|\[|\]|\*|\/|\r\n|\n|\r|[]|)/gm, "");
-      console.log(`normalise = ${normalise}`);
+      let normalise = string.replace(/(\;|\]|\%|\*|\/|\[|\r\n|\n|\r)/gm, ",");
+      if (string.includes("-")) throw new Error("negatives not allowed");
+      if (normalise == "") return 0;
+      else if (normalise.length === 1) return parseInt(normalise, 10);
+      else arr = normalise.split(",");
+      let sum = 0;
+      // arr.map((element,index) => {
+      //   if (element == "" || element >= 1001) {
+      //     arr.splice(index, 1)
+      //     console.log(index);
+      //   }
+      //   console.log(arr);
+      // })
 
-      if (string.includes(""))
+      for (let j = 0; j < arr.length; j++)
+        if (arr[j] >= 1001 || arr[j] == "") {
+          arr.splice(j, 1);
+          j--;
+        }
 
-      if (string.includes("-"))
-        throw new Error("negatives not allowed");
-
-      if(normalise == "")
-        return 0;
-      else if(normalise.length === 1)
-        return parseInt(normalise, 10);
-      else
-        split = normalise.split('');
-        console.log(`split = ${split}`);
-        let sum = 0;
-        let arrlength = split.length;
-        console.log(`arrlength = ${arrlength}`);
-        for (let i = 0; i < arrlength; i++)
-          sum += parseInt(split[i]);
-        return sum; 
-    }  
+      for (let i = 0; i < arr.length; i++) sum += parseInt(arr[i]);
+      return sum;
+    };
   }
 }
-let string = new StringCalculator;
-//console.log(string.add("-"));
-// console.log(string.add("1\n2,3"));
-// console.log(string.add("1,2"));
-// randomArray = (length, max) => [...new Array(length)].map(() => Math.round(Math.random() * max));
-// randy = Math.floor(Math.random() * 100 + 1);
-// res = randomArray(randy, randy);
-// sum = res.reduce((pv, cv) => pv + cv, 0);
-// arg = res.join();
-// str = "//[***]\n1***2***3"
-// console.log(string.add(str));
-console.log(string.add("//[*][%]\n1*2%3"));
+let string = new StringCalculator();
--- stringCalculatorSpec.js
+++ stringCalculatorSpec.js
@@ -1,17 +1,17 @@
-describe("String string", function() {
-  it("should return zero when '' is passed", function() {
+describe("String Calculator", () => {
+  it("should return zero when '' is passed", () => {
     expect(string.add("")).toEqual(0);
   });
 
-  it("should return the number itself when a single number is passed", function() {
+  it("should return the number itself when a single number is passed", () => {
     expect(string.add("1")).toEqual(1);
   });
 
-  it("should return the sum of the numbers if two are given", function() {
+  it("should return the sum of the numbers if two are given", () => {
     expect(string.add("1,2")).toEqual(3);
   });
 
-  it("should return the sum of an unknown amount of numbers", function() {
+  it("should return the sum of an unknown amount of numbers", () => {
     randomArr = (length, max) => [...new Array(length)].map(() => Math.round(Math.random() * max));
     randonGen = Math.floor(Math.random() * 100 + 1);
     res = randomArr(randonGen, randonGen);
@@ -20,7 +20,7 @@ describe("String string", function() {
     expect(string.add(arg)).toEqual(sum);
   });
 
-  it("should allow \\n in between the input number string", function() {
+  it("should allow the Add method to handle new lines between numbers (instead of commas)", () => {
     expect(string.add("1\n2,3")).toEqual(6);
   });
 
@@ -29,4 +29,25 @@ describe("String string", function() {
       string.add("-1,2,3");
     }).toThrow(new Error("negatives not allowed"));
   });
+  
+  it("should ignore all numbers greater than 1000", () => {
+    expect(string.add("2,1001")).toEqual(2);
+  })
+
+  it("should support different delimeters", () => {
+    expect(string.add("//;\n1;2")).toEqual(3);
+  });
+
+  it("should handle delimeters of any length", () => {
+    expect(string.add("//[***]\n1***2***3")).toEqual(6);
+  });
+
+  it("should handle multiple delimeters", () => {
+    expect(string.add("//[*][%]\n1*2%3")).toEqual(6);
+  });
+
+  it("should handle multiple delimiters with length longer than one char", () => {
+    expect(string.add("//[*][%%%%%%%%%%]]]]]]\n1*2%3")).toEqual(6);
+  });
+
 });
