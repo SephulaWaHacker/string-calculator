258dd3e05df350a2b413ed99e7d64ef66309b47f
Sephula Mokoena <49900319+SweetBeard30@users.noreply.github.com>
2019-07-02 11:11:45 +0200

    Update stringCalculator.js

--- stringCalculator.js
+++ stringCalculator.js
@@ -9,20 +9,11 @@ class StringCalculator {
       else if (normalise.length === 1) return parseInt(normalise, 10);
       else arr = normalise.split(",");
       let sum = 0;
-      // arr.map((element,index) => {
-      //   if (element == "" || element >= 1001) {
-      //     arr.splice(index, 1)
-      //     console.log(index);
-      //   }
-      //   console.log(arr);
-      // })
-
       for (let j = 0; j < arr.length; j++)
         if (arr[j] >= 1001 || arr[j] == "") {
           arr.splice(j, 1);
           j--;
         }
-
       for (let i = 0; i < arr.length; i++) sum += parseInt(arr[i]);
       return sum;
     };
